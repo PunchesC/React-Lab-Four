da2f20564e08e027fa34d61aeb9f5899dab74c47
Curtis <c.b.punches@gmail.com>
2021-05-18 09:01:25 -0400

    working on repo

--- src/Components/Repository.tsx
+++ src/Components/Repository.tsx
@@ -1,37 +1,47 @@
 import './Repository.css'
 
-// import {User, UserRepository} from '../model/github-model';
-// import { useEffect, useState } from "react";
-// import {fetchAllUserRepo, fetchUserProfile} from "../service/GitHubApiService";
+import {User, UserRepository} from '../model/github-model';
+import { useEffect, useState } from "react";
+import {fetchAllUserRepo,fetchUserProfile} from "../service/GitHubApiService";
 
 
 
 
 
-// function Repository() {
-// const [repo,setRepo]=useState<UserRepository[]>([])
+function Repository() {
+const [repo,setRepo]=useState<UserRepository[]>([])
+const [singleUser, setSingleUser] =useState<User|null>(null);
+const [selectedName, setSelectedName] = useState("PunchesC");
 
-//   return (
-//     <div className="Repository">
-     
-//     </div>
-//   )
-// }
+useEffect(()=>{
+  if(selectedName){
+  fetchUserProfile(selectedName).then(data=>{
+    setSingleUser(data);
+  });
+} 
+}, [selectedName]);
+  
 
-// export default Repository;
+useEffect(()=>{
+  fetchAllUserRepo(selectedName).then(data=>{
+    setRepo(data);
+  });
+}, []);
+  return (
+    <div className="Repository">
+     <h2>Repository List</h2>
+    { repo.length == 0 ?
+      <p>Loading data...</p> :
+      <ol>
+        {/* As usual, render array using .map */}
+        {repo.map((userrepo, i) => 
+          <li key={i}>{userrepo.name} {userrepo.html_url} {userrepo.description }{userrepo.description} {userrepo.language} {userrepo.push_at}</li>
+         
+        )}
+      </ol> }
+    </div>
+  )
+}
 
+export default Repository;
 
-// // const [selectedName, setSelectedName] = useState("");
-// // const [singleUser, setSingleUser] =useState<User|null>(null);
-
-// // useEffect(()=>{
-// //   if(selectedName){
-// //   fetchUserProfile(selectedName).then(data=>{
-// //     setSingleUser(data);
-// //   });
-// // } else {
-// //   setSingleUser(null);
-// // }
-// // }, [selectedName]);
-
-// // console.log(selectedName)
--- src/Components/UserLookup.tsx
+++ src/Components/UserLookup.tsx
@@ -1,13 +1,13 @@
 import './UserLookup.css'
 import GitHubUserSummary from "./GitHubSummary"
-// import Repository from './Repository'
+import Repository from './Repository'
 
 
 function UserLookup(){
   return (
     <div>
       <GitHubUserSummary />
-      {/* <Repository /> */}
+      <Repository />
     </div>
   )
 }
